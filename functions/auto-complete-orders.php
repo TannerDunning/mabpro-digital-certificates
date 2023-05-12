b095961cc8682728a761593a6776324c6190746a
Replit user <>
2023-05-12 22:57:59 +0000

    fixes issue with processing orders and autocompleting /nix/store/8kgsjv57icc18qhpmj588g9x1w34hi4j-bash-interactive-5.1-p12/bin/bash total payments.

--- functions/auto-complete-orders.php
+++ functions/auto-complete-orders.php
@@ -1,13 +1,8 @@
 <?php
 
-/**
- * Auto-completes orders that have a total of $0.
- */
-
-add_action( 'woocommerce_order_status_changed', function( $order_id, $old_status, $new_status ) {
-
+add_action('woocommerce_order_status_changed', function ($order_id, $old_status, $new_status) {
     // Get the order.
-    $order = wc_get_order( $order_id );
+    $order = wc_get_order($order_id);
 
     // Only complete orders that have a total of $0 and are in the "processing" status.
     if (
@@ -15,10 +10,8 @@ add_action( 'woocommerce_order_status_changed', function( $order_id, $old_status
         && $order->is_paid()
         && $order->get_status() == 'processing'
     ) {
-
         // Update the order status to "completed".
-        $order->update_status( 'completed' );
+        $order->update_status('completed');
     }
-});
-
+}, 10, 3);
 ?>
