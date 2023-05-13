47d200d3f783d740af735d0f41a111abeab1ca29
Replit user <>
2023-05-13 00:27:28 +0000

    added error logging for #4

--- functions/add-balance-field.php
+++ functions/add-balance-field.php
@@ -1,6 +1,7 @@
 <?php
 
 function add_cert_balance_field( $order_id ) {
+    error_log('add_cert_balance_field function started'); // Log the start of the function
     $order = wc_get_order( $order_id ); // Get the order object
     $items = $order->get_items(); // Get the items in the order
     foreach ( $items as $item ) {
@@ -9,12 +10,14 @@ function add_cert_balance_field( $order_id ) {
         if ( $product_id == 34491 ) { // Replace 34491 with the ID of your token product
             $user_id = $order->get_user_id(); // Get the user ID
             $cert_balance = get_user_meta( $user_id, 'cert_balance', true ); // Get the current balance
+            error_log('Token product found, current balance: ' . $cert_balance); // Log the current balance
             if ( ! $cert_balance ) {
                 $cert_balance = 0;
             }
             $quantity = $item->get_quantity(); // Get the quantity of tokens purchased
             $cert_balance += $quantity; // Add the quantity of tokens to the balance
             update_user_meta( $user_id, 'cert_balance', $cert_balance ); // Update the balance field in the user profile
+            error_log('New balance: ' . $cert_balance); // Log the new balance
         }
     }
 }
