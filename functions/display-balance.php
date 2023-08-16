afce8b3a7ea8b5a97580e3d3bddfbc49db751a06
Replit user <>
2023-08-16 20:22:59 +0000

    added css to token balance and removed balance text

--- functions/display-balance.php
+++ functions/display-balance.php
@@ -1,4 +1,12 @@
 <?php
+echo '<style>
+    .balance_number {
+        color: #FFF; /* White color */
+        font-size: 32px; /* Font size */
+        font-weight: bold; /* Bold text */
+    }
+</style>';
+
 function display_instructor_cert_balance() {
     error_log('display_instructor_cert_balance function started'); // Log the start of the function
     $user_id = get_current_user_id();
@@ -10,8 +18,8 @@ function display_instructor_cert_balance() {
         return '<div class="cert_balance_message">You currently have no certificate tokens. <a href="https://testmabpro.wpengine.com/product/certificate-tokens/">Purchase tokens here</a>.</div>';
     }
 
-    // If there is a cert_balance meta field, return the balance
-    return '<div class="cert_balance_message">Your current certificate balance is: <span class="balance_number">' . $balance . '</span></div>';
+    // If there is a cert_balance meta field, return the balance wrapped in a span with the class "balance_number"
+    return '<span class="balance_number">' . $balance . '</span>';
 }
 add_shortcode( 'instructor_cert_balance', 'display_instructor_cert_balance' );
 ?>
